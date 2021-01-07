66ffdac7345172b1e38c79d56ef4d1499b9cd83a
mgbams <cmgbams@gmail.com>
2021-01-07 03:18:51 +0100

    retrieval of user data

--- src/components/ExperienceLists.js
+++ src/components/ExperienceLists.js
@@ -1,12 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
 import SingleExperience from "./SingleExperience";
 const url = "https://course-api.com/react-tabs-project";
 
 const ExperienceLists = () => {
+  const [user, setUser] = useState([]);
+  const [initialData, setInitialData] = useState();
+
+  const fetchUser = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    setUser(data);
+    setInitialData(data[0]);
+    console.log(user);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-    <div>
-      <SingleExperience />
+    <div className="container-sm">
+      <div className="row d-flex flex-column justify-content-center align-items-center">
+        <h1>Experience</h1>
+        <div
+          style={{ width: "100px", height: "4px", backgroundColor: "green" }}
+        ></div>
+      </div>
+
+      <div className="row d-flex justify-content-between mt-5">
+        <div className="col-2">
+          {user.map((user) => {
+            return (
+              <li
+                style={{ listStyleType: "none" }}
+                key={user.id}
+                className="d-flex mb-3"
+              >
+                {" "}
+                <div
+                  style={{
+                    width: "3px",
+                    height: "40px",
+                    backgroundColor: "green",
+                  }}
+                  className="mr-5"
+                ></div>{" "}
+                <p>{user.company ? user.company : ""}</p>
+              </li>
+            );
+          })}
+        </div>
+        <div className="col-10">
+          <SingleExperience {...initialData} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SingleExperience.js
+++ src/components/SingleExperience.js
@@ -1,8 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-const SingleExperience = (props) => {
-  return <div>Single experience</div>;
+const SingleExperience = ({ company, id, order, title, dates, duties }) => {
+  return (
+    <div className="d-flex flex-column justify-content-start">
+      <h2 className="job-title">{title ? title : ""}</h2>
+      <span className="text-uppercase">tommy</span>
+      <br />
+      <span>{dates ? dates : ""}</span>
+      <br />
+      <li style={{ listStyleType: "none" }}>
+        <FaAngleDoubleRight style={{ color: "green" }} />
+        &nbsp; Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke
+        leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge
+        disrupt.
+      </li>
+      <button
+        style={{ width: "150px", marginLeft: "200px" }}
+        type="button"
+        className="text-uppercase text-white btn btn-info mt-5"
+      >
+        more info
+      </button>
+    </div>
+  );
 };
 
 SingleExperience.propTypes = {};
